0cfd28eb9bf0d2f7d02f52bf2502064733144d15
Rami CUD <20220001825@students.cud.ac.ae>
2024-12-16 15:04:35 +0400

    First commit

---
+++ main.py
@@ -0,0 +1,294 @@
+import imgui
+import glfw
+from imgui.integrations.glfw import GlfwRenderer
+import OpenGL.GL as gl
+import sys
+from datetime import time, timedelta, datetime
+import parser
+from parser import Course, CourseSection, CourseSession, TimeRange, Schedule
+import pickle
+from requests import Session
+from appdirs import user_data_dir
+import os
+
+if hasattr(sys, '_MEIPASS'):  # PyInstaller's temp directory
+    os.environ['PATH'] += os.pathsep + sys._MEIPASS
+
+data_dir = user_data_dir("SchedulerV2", False)
+os.makedirs(data_dir, exist_ok=True)
+PICKLE_PATH = os.path.join(data_dir, "all_courses.pickle")
+
+
+def impl_glfw_init():
+    width, height = 1600, 900
+    window_name = "SchedulerV2"
+
+    if not glfw.init():
+        print("Could not initialize OpenGL context")
+        sys.exit(1)
+
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+
+    window = glfw.create_window(
+        int(width), int(height), window_name, None, None)
+    glfw.make_context_current(window)
+
+    if not window:
+        glfw.terminate()
+        print("Could not initialize Window")
+        sys.exit(1)
+
+    return window
+
+
+week_day = {1: "M", 2: "T", 3: "W", 4: "R", 5: "F"}
+
+
+class FilteredComboList():
+    def __init__(self, label, index=0, items=None):
+        self.label = label
+        self.index = index
+        self.items: dict[Course, list[CourseSection]] = items
+        self.filter_text = ""
+        self.selected_items = {}
+        self.set_text_focus = False
+
+    def show(self):
+        IO = imgui.get_io()
+        if (IO.key_ctrl and IO.keys_down[glfw.KEY_E]) or self.set_text_focus:
+            imgui.set_keyboard_focus_here()
+            self.set_text_focus = False
+        imgui.push_item_width(300)
+        changed, self.filter_text = imgui.input_text(f"##{self.label}_filter", self.filter_text, 256)
+        if changed:
+            self.index = 0
+        filtered_items = [item for item in self.items if self.filter_text.lower() in item.name.lower()]
+        filtered_item_names = [item.name for item in self.items if self.filter_text.lower() in item.name.lower()]
+        clicked, selected_index = imgui.combo(f"##{self.label}", self.index, filtered_item_names)
+        imgui.pop_item_width()
+        if clicked:
+            self.index = selected_index
+        if imgui.button(f"Add Course", 200, 20) or imgui.is_key_pressed(glfw.KEY_ENTER):
+            selected_course = filtered_items[selected_index]
+            self.selected_items[selected_course] = self.items[selected_course]
+            self.filter_text = ""
+            self.set_text_focus = True
+
+        available_width = imgui.get_content_region_available_width()
+        current_width = 0
+        for i, selected_item in enumerate(self.selected_items):
+            current_width = current_width + imgui.calc_text_size(selected_item.name)[0] + 20
+            if current_width > available_width:
+                imgui.new_line()
+                current_width = 0
+            if imgui.button(selected_item.name):
+                del self.selected_items[selected_item]
+                break
+            if i != len(self.selected_items)-1:
+                imgui.same_line()
+        if imgui.button("Clear Courses", 200, 20):
+            self.selected_items = {}
+
+    def getSectionsForEachCourse(self):
+        output_list: list[list[CourseSection]] = []
+        for course, course_sections in self.selected_items.items():
+            output_list.append(course_sections)
+        return output_list
+
+
+class DaysFilter:
+    def __init__(self):
+        self.is_range = False
+        self.selected_days = 4
+
+    def show(self):
+        imgui.text("Day Filter: ")
+        imgui.same_line()
+        imgui.push_item_width(100)
+        clicked, selected_index = imgui.combo("##rangeOrExact", int(self.is_range), ["Exactly", "Atleast"])
+        if clicked:
+            self.is_range = False if selected_index == 0 else True
+        imgui.same_line()
+        days_range = [str(i) for i in range(1, 6)]
+        clicked, selected_index = imgui.combo("##days_selected", self.selected_days-1, days_range)
+        if clicked:
+            self.selected_days = selected_index + 1
+        imgui.pop_item_width()
+        imgui.same_line()
+        imgui.text("days")
+
+
+class Tables:
+    def __init__(self):
+        self.show_tables = False
+        self.schedules: dict[Schedule] = {}
+
+    def show(self):
+        if imgui.begin_tab_bar("Main"):
+            for i, scheule in enumerate(self.schedules, start=1):
+                if imgui.begin_tab_item(f"Schedule {i}")[0]:
+                    self._display_sched_table(scheule)
+                    imgui.end_tab_item()
+            imgui.end_tab_bar()
+
+    def _display_sched_table(self, schedule):
+        available_width, available_height = imgui.get_content_region_available()
+        if imgui.begin_table(f"schedule", 6, imgui.TABLE_BORDERS | imgui.TABLE_ROW_BACKGROUND, outer_size_height=available_height, outer_size_width=available_width):
+            imgui.table_setup_column("Time")
+            imgui.table_setup_column("Monday")
+            imgui.table_setup_column("Tuesday")
+            imgui.table_setup_column("Wednesday")
+            imgui.table_setup_column("Thursday")
+            imgui.table_setup_column("Friday")
+            imgui.table_headers_row()
+            for row in range(25):
+                imgui.table_next_row()
+                for col in range(6):
+                    imgui.table_next_column()
+                    if col == 0:
+                        set_time = (datetime.combine(datetime.today(), time(hour=9)) + timedelta(minutes=30*row)).time()
+                        imgui.text(set_time.strftime("%I:%M %p"))
+                    else:
+                        if (cell_item := self.schedules[schedule][row][col]) is not None:
+                            imgui.text(f"{cell_item.section_id} {cell_item.course.name}")
+            imgui.end_table()
+
+    def _get_course_at_this_time(self, given_schedule: Schedule, given_time: time, day: str):
+        for section in given_schedule.course_sections:
+            for session in section.sessions:
+                on_the_same_day = set(day).intersection(set(session.days))
+                is_within_given_time = session.timing.within_range(given_time)
+                if on_the_same_day and is_within_given_time:
+                    return section
+
+    def generate_table_positions(self):
+        for schedule in self.schedules:
+            positions = [[None] * 6 for _ in range(25)]
+            for row, _ in enumerate(positions):
+                for col, _ in enumerate(_):
+                    if col == 0:
+                        continue
+                    current_time = (datetime.combine(datetime.today(), time(hour=9)) + timedelta(minutes=30*row)).time()
+                    current_day = week_day[col]
+                    positions[row][col] = self._get_course_at_this_time(schedule, current_time, current_day)
+            self.schedules[schedule] = positions
+
+    def filter_schedules(self, is_range, days):
+        new_schedules_dict = {}
+        if is_range:
+            def passes(given_day, target_day): return given_day <= target_day
+        else:
+            def passes(given_day, target_day): return given_day == target_day
+        for schedule in self.schedules:
+            if passes(schedule.days, days):
+                new_schedules_dict[schedule] = None
+        self.schedules = new_schedules_dict
+
+
+class ImportSection:
+    def __init__(self):
+        self.departments = ['ACT', 'ADV', 'AIMC', 'APS', 'ARC', 'BCS', 'BIO', 'BSD', 'BSM', 'BUS', 'CMN', 'COM', 'CRI', 'CYS', 'DES', 'DMJ', 'EAP', 'EBU', 'ECO', 'ELC', 'ENG', 'ENT', 'ENV', 'FIN', 'GED', 'HA',
+                            'HOM', 'HRM', 'IDF', 'INB', 'IND', 'JRN', 'LNG', 'MATH', 'MBA', 'MCM', 'MEC', 'MGT', 'MIT', 'MKT', 'MTH', 'NET', 'NNS', 'PHYS', 'PLX', 'PRA', 'PSY', 'QBA', 'RTA', 'SCI', 'SCM', 'SHS', 'SOC', 'SPT', 'SWS']
+        self.selected_index = 0
+        self.session: Session = None
+
+    def show(self):
+        _, self.selected_index = imgui.combo("##departments", self.selected_index, self.departments)
+        if imgui.button("Import Sections"):
+            if self.session is None:
+                self.session = parser.get_session()
+            return self.update_pickle(self.departments[self.selected_index])
+
+    def update_pickle(self, department: str) -> dict:
+        new_course_dict, self.departments = parser.get_all_courses_in_department(self.session, department)
+        try:
+            with open(PICKLE_PATH, "rb") as file:
+                old_course_dict = pickle.load(file)
+        except OSError:
+            old_course_dict = {}
+        with open(PICKLE_PATH, "wb") as file:
+            old_course_dict.update(new_course_dict)
+            pickle.dump(old_course_dict, file)
+        return old_course_dict
+
+
+try:
+    with open(PICKLE_PATH, "rb") as file:
+        courses_dict = pickle.load(file)
+except OSError:
+    courses_dict = {}
+course_selector = FilteredComboList("list", 0, courses_dict)
+days_filter = DaysFilter()
+tables = Tables()
+import_section = ImportSection()
+
+
+def frame_commands():
+    imgui.set_next_window_size(630, 370, condition=imgui.ONCE)
+    if imgui.begin("Scheduler"):
+        # error_popup.show()
+        imgui.columns(2, border=False)
+        course_selector.show()
+        days_filter.show()
+        if imgui.button("Generate Tables"):
+            schedules_dict = {schedule: None for schedule in parser.create_all_possible_schedules(course_selector.getSectionsForEachCourse())}
+            tables.schedules = schedules_dict
+            tables.filter_schedules(days_filter.is_range, days_filter.selected_days)
+            tables.generate_table_positions()
+            if tables.schedules:
+                tables.show_tables = True
+        if tables.show_tables:
+            imgui.set_next_window_size(1300, 540, condition=imgui.ONCE)
+            _, opened = imgui.begin("##Schedules", closable=True)
+            if opened:
+                tables.show()
+                imgui.end()
+            else:
+                tables.show_tables = False
+                imgui.end()
+        imgui.next_column()
+        if new_dict := import_section.show():
+            course_selector.items = new_dict
+        imgui.end()
+
+
+def render_frame(impl, window, font=None):
+    glfw.poll_events()
+    impl.process_inputs()
+    imgui.new_frame()
+
+    gl.glClearColor(0.1, 0.1, 0.1, 1)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+    if font is not None:
+        imgui.push_font(font)
+    frame_commands()
+    if font is not None:
+        imgui.pop_font()
+
+    imgui.render()
+    impl.render(imgui.get_draw_data())
+    glfw.swap_buffers(window)
+
+
+def main():
+    imgui.create_context()
+    window = impl_glfw_init()
+
+    impl = GlfwRenderer(window)
+
+    io = imgui.get_io()
+    impl.refresh_font_texture()
+
+    while not glfw.window_should_close(window):
+        render_frame(impl, window)
+
+    impl.shutdown()
+    glfw.terminate()
+
+
+if __name__ == "__main__":
+    main()
---
+++ parser.py
@@ -0,0 +1,242 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup, element
+from dataclasses import dataclass
+from datetime import time
+from collections import defaultdict
+import re
+from datetime import time, datetime
+import copy
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TimeRange:
+    def __init__(self, start: time, end: time):
+        self.start: time = start
+        self.end: time = end
+
+    def overlaps(self, other: "TimeRange") -> bool:
+        if not isinstance(other, self.__class__):
+            raise ValueError("Can only compare with other time ranges")
+        return not (self.end <= other.start or other.end <= self.start)
+
+    def within_range(self, time_period: time):
+        return self.start <= time_period < self.end
+
+    @classmethod
+    def parse_from_string(cls, start_time: str, end_time: str):
+        start = datetime.strptime(start_time, "%I:%M:%S %p").time()
+        end = datetime.strptime(end_time, "%I:%M:%S %p").time()
+        return cls(start, end)
+
+    def __repr__(self):
+        return f"'TimeRange({self.start}, {self.end})'"
+
+
+@dataclass(frozen=True, order=True)
+class Course:
+    course_id: str
+    department: str
+    name: str
+    credit_hours: int
+
+
+@dataclass(frozen=True, order=True)
+class CourseSession:
+    instructor: str
+    room: str
+    days: str
+    timing: TimeRange
+
+
+@dataclass(frozen=True, order=True)
+class CourseSection:
+    course: Course
+    section_id: str
+    sessions: tuple[CourseSession]
+
+
+class Schedule:
+    def __init__(self, course_sections: set[CourseSection] = None):
+        self.course_sections = course_sections if course_sections is not None else set()
+        self.days = 0
+
+    def conflicts(self, other_section: CourseSection):
+        for section in self.course_sections:
+            for session in section.sessions:
+                for other_session in other_section.sessions:
+                    shared_days = set(other_session.days).intersection(
+                        set(session.days))
+                    if shared_days and other_session.timing.overlaps(session.timing):
+                        return True
+        return False
+
+    def add_section(self, section: CourseSection):
+        if self.conflicts(section):
+            raise ValueError("Provided section conflicts with Schedule")
+        self.course_sections.add(section)
+
+    def calculate_days(self):
+        days = set()
+        for section in self.course_sections:
+            for session in section.sessions:
+                days.add(session.days)
+        self.days = len(days)
+
+
+def create_all_possible_schedules(sections: list[list[CourseSection]]) -> list[Schedule]:
+    def recursively_find_schedules(sections: list[list[CourseSection]], schedules: list[Schedule], current_schedule: Schedule, index: int = 0):
+        if index == len(sections):
+            current_schedule.calculate_days()
+            schedules.append(copy.deepcopy(current_schedule))
+            current_schedule = Schedule()
+            return
+
+        sections_of_one_course = sections[index]
+        for section in sections_of_one_course:
+            if current_schedule.conflicts(section):
+                continue
+            current_schedule.add_section(section)
+            recursively_find_schedules(
+                sections, schedules, current_schedule, index+1)
+            current_schedule.course_sections.remove(section)
+    schedules = []
+    initial_schedule: Schedule = Schedule()
+    recursively_find_schedules(sections, schedules, initial_schedule)
+    return schedules
+
+
+def get_session() -> requests.Session:
+    session = requests.Session()
+    # Configure Selenium WebDriver
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")  # Optional: run in headless mode
+    chromedriver = ChromeDriverManager()
+    driver = webdriver.Chrome(service=Service(
+        chromedriver.install()), options=chrome_options)
+
+    login_url = "https://cudportal.cud.ac.ae/student/login.asp"
+    # Open login page
+    driver.get(login_url)
+
+    try:
+        WebDriverWait(driver, 300).until_not(EC.url_contains(login_url))
+        # Get cookies from the browser
+        selenium_cookies = driver.get_cookies()
+
+        # Add cookies to requests.Session
+        for cookie in selenium_cookies:
+            session.cookies.set(cookie['name'], cookie['value'])
+    finally:
+        driver.quit()
+    return session
+
+
+def get_all_courses_in_department(session: requests.Session, department: str) -> tuple[dict, list[str]]:
+    def get_page(num_pages): return get_page_in_department(
+        session, num_pages, department)
+    response = get_page(1)
+    first_page_soup = BeautifulSoup(response.text, "html.parser")
+    departments_list = get_departments(first_page_soup)
+    num_of_pages = get_pages_num(first_page_soup)
+    soups: list[BeautifulSoup] = [first_page_soup]
+    for i in range(2, num_of_pages+1):
+        soups.append(BeautifulSoup(get_page(i).text, "html.parser"))
+    courses: dict[Course, list[CourseSection]] = defaultdict(list)
+    for soup in soups:
+        table_element = soup.find(
+            "table", class_="Portal_Group_Table topLevel")
+        all_course_infos = table_element.find_all("tr", class_="courseInfo")
+        for info_index, course_info in enumerate(all_course_infos):
+            info_elements = course_info.find_all("td")
+            course_full_id = info_elements[0].get_text(strip=True)
+            course_id = course_full_id[:course_full_id.find("Lec")]
+            course_name = info_elements[1].get_text(strip=True)
+            credit_hours = int(info_elements[2].get_text(strip=True))
+            course = Course(course_id, re.sub(
+                r"\d+", "", course_id), course_name, credit_hours)
+            if course in courses:
+                continue
+            for i, table in enumerate(table_element.find_all("table", class_="Portal_Group_Table nested", summary=f"{course_name} Schedule")):
+                sessions: list[CourseSession] = []
+                for row in table.find_all("tr", class_=""):
+                    cells = row.find_all("td")
+                    instructor = cells[1].get_text(strip=True)
+                    room = cells[2].get_text(strip=True)
+                    days = cells[3].get_text(strip=True)
+                    start_time = cells[5].get_text(strip=True)
+                    end_time = cells[6].get_text(strip=True)
+                    time_range = TimeRange.parse_from_string(
+                        start_time, end_time)
+                    sessions.append(CourseSession(
+                        instructor, room, days, time_range))
+                course_full_id = all_course_infos[i+info_index].find("td").get_text(strip=True)
+                courses[course].append(CourseSection(
+                    course, course_full_id, tuple(sessions)))
+    return (courses, departments_list)
+
+
+def get_page_in_department(session: requests.Session, page_num, department) -> requests.Response:
+    URL = r"https://cudportal.cud.ac.ae/student/cePortalOffering.asp"
+    form_data = {
+        "IsPostBack": "True",
+        "f_TermCalendarID": "71",  # Example: term identifier
+        "f_Days": "",  # Days filter
+        "f_TimeFrom": "",  # Time range start
+        "f_TimeTo": "",  # Time range end
+        "f_Campuses": "",  # Campuses filter
+        "f_Departments": department,  # Example: a department code
+        "f_Divisions": "",  # Division filter
+        "page": page_num,  # Start with page 1
+    }
+    response = session.post(URL, data=form_data)
+    if response.status_code != 200:
+        raise RuntimeError("Couldn't get correct response")
+    return response
+
+
+def get_pages_num(soup: BeautifulSoup) -> int:
+    line = soup.find("div", class_="Portal_Grid_Pager").get_text(strip=True)
+    i = line.find(": ") + 2
+    try:
+        num = int(line[i::i+1])
+    except Exception as e:
+        raise e
+    return num
+
+
+def get_departments(soup: BeautifulSoup) -> list[str]:
+    options: list[element.Tag] = soup.find("select", id="idDepartments").find_all("option")
+    option_str = [option.get_text(strip=True) for option in options]
+    return option_str
+
+# result = get_all_courses_in_department(get_session(), "BCS")
+# with open("all_courses.pickle", "wb") as file:
+#     pickle.dump(result, file)
+# with open("test.txt", "w") as file:
+#     pprint.pprint(result, file)
+# with open("all_courses.pickle", "wb") as file:
+#     pickle.dump(get_all_courses_in_department(get_session(), "BCS"), file)
+
+# with open("test.txt", "w") as file:
+#     pprint.pprint(get_all_courses_in_department(get_session(), "BCS"), file)
+
+# with open("all_courses.pickle", "rb") as pick:
+#     BCS_courses: dict[Course, list[CourseSection]] = pickle.load(pick)
+
+# selected_courses = ["BCS306", "BCS307", "BCS309", "BCS323"]
+# selected_sections = []
+# for course, sections in BCS_courses.items():
+#     if course.course_id in selected_courses:
+#         selected_sections.append(sections)
+# x = create_all_possible_schedules(selected_sections)
+# print("Put a breakpoint here :D")
+
+
+# sess = get_session()
+# print("Put a breakpoint here :D")
